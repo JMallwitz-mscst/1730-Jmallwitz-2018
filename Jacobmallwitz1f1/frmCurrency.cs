3e3afc84926c9ac31445efc961a571b45ebc023b
Jacob Mallwitz <ae7277kc@go.minnstate.edu>
2018-10-02 06:26:09 -0500

    Exercise 2b1 Completed

--- Jacobmallwitz1f1/frmCurrency.cs
+++ Jacobmallwitz1f1/frmCurrency.cs
@@ -19,7 +19,7 @@ namespace Jacobmallwitz1f1
 
         private void picKoreaDim_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void frmCurrency_Load(object sender, EventArgs e)
@@ -28,7 +28,7 @@ namespace Jacobmallwitz1f1
             btnAustralia.BackgroundImage = picAustraliaDim.Image;
             btnJapan.BackgroundImage = picJapanDim.Image;
             btnKorea.BackgroundImage = picKoreaDim.Image;
-            lblCurrency.Text = btnBrazil.Text + ": ";
+            lblCurrency.Text = "0.00";
             txtCurrency.Text = "0.00";
             txtRate.Text = "0.24";
             txtUSD.Text = "0.00";
@@ -57,7 +57,7 @@ namespace Jacobmallwitz1f1
             btnBrazil.BackgroundImage = picBrazilDim.Image;
             btnJapan.BackgroundImage = picJapanDim.Image;
             btnKorea.BackgroundImage = picKoreaDim.Image;
-            lblCurrency.Text = btnAustralia.Text + ": ";
+            lblCurrency.Text = txtRate.Text;
             txtCurrency.Text = "0.00";
             txtRate.Text = "0.72";
             txtUSD.Text = "0.00";
@@ -89,13 +89,13 @@ namespace Jacobmallwitz1f1
             txtCurrency.Text = "0.00";
             txtRate.Text = "0.00089";
             txtUSD.Text = "0.00";
-            txtTotal.Text = "0.00";
+            txtTotal.Text = (4 - txtRate.TabIndex).ToString();
             txtCurrency.Focus();
         }
 
         private void calcUSD(object sender, EventArgs e)
         {
-            
+           
         }
 
         private void txtCurrency_Enter(object sender, EventArgs e)
@@ -109,8 +109,8 @@ namespace Jacobmallwitz1f1
                                Convert.ToDecimal(txtRate.Text)).ToString("0.00");
             txtTotal.Text = txtUSD.Text;
             lblEquation.Text = lblEquation.Text + " + " + txtTotal.Text;
-        }
-
+            txtRate.Text = txtUSD.Text + txtTotal.Text;
+                }
         private void btnReset_Click(object sender, EventArgs e)
         {
             txtCurrency.Text = "0.00";
---
+++ jmallwitz2b1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jmallwitz2b1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate(object sender, EventArgs e)
+        {
+            decimal AmountAustralia = Convert.ToDecimal(txtAmountAustralia.Text);
+            decimal Australianrate = 0.72m;
+            decimal AustraliaUSD = AmountAustralia * Australianrate;
+            decimal AmountBrazil = Convert.ToDecimal(txtAmountBrazil.Text);
+            decimal Brazilrate = 0.25m;
+            decimal BrazilUSD = AmountBrazil * Brazilrate;
+            decimal AmountJapan = Convert.ToDecimal(txtAmountJapan.Text);
+            decimal Japanrate = 0.0088m;
+            decimal JapanUSD = AmountJapan * Japanrate;
+            decimal Koreaamount = Convert.ToDecimal(txtKoreaamount.Text);
+            decimal Korearate = 0.00089m;
+            decimal KoreaUSD = Koreaamount * Korearate;
+            decimal USDTotal = AustraliaUSD + BrazilUSD + JapanUSD + KoreaUSD;
+            txtAustraliaUSD.Text = Convert.ToDecimal(AustraliaUSD).ToString("0.00");
+            txtBrazilUSD.Text = Convert.ToDecimal(BrazilUSD).ToString("0.00");
+            txtJapanUSD.Text = Convert.ToDecimal(JapanUSD).ToString("0.00");
+            txtKoreaUSD.Text = Convert.ToDecimal(KoreaUSD).ToString("0.00");
+            txtUSDTotal.Text = Convert.ToDecimal(USDTotal).ToString("0.00");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtAmountAustralia.Text = "0.00";
+            txtAmountBrazil.Text = "0.00";
+            txtAmountJapan.Text = "0.00";
+            txtKoreaamount.Text = "0.00";
+            txtAustraliaUSD.Text = "0.00";
+            txtBrazilUSD.Text = "0.00";
+            txtJapanUSD.Text = "0.00";
+            txtKoreaUSD.Text = "0.00";
+            txtUSDTotal.Text = "0.00";
+        }
+    }
+}
