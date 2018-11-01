3c568d13d9ed8164019178971a2c2318b6ccc4bd
Jacob Mallwitz <ae7277kc@go.minnstate.edu>
2018-10-31 23:31:15 -0500

    exercise 2f1 complete

---
+++ jmallwitz2fUnitTests.cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jmallwitz2fUnitTests.cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("jmallwitz2fUnitTests.cs")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8324aea0-1d0c-43f5-8045-3ca796b7f34f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
