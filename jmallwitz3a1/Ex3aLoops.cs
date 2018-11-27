b18edc355e7d8ee43f856da3f4931254d332ed4d
Jacob Mallwitz <ae7277kc@go.minnstate.edu>
2018-11-26 23:51:57 -0600

    ex3a1 complete

---
+++ jmallwitz3a1/Ex3aLoops.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jmallwitz3a1
+{
+    public class Ex3aLoops
+    {
+        //public static string Calc0()
+        //{
+        //    int i = 1, sum = 0;
+        //    while (i < 5)
+        //    {
+        //        sum += i;
+        //        i++;
+        //    }
+        //    return sum.ToString();
+        //}
+        public static string Calc0(string strMonths, string strMonthlyPayment, string strMonthlyInterestRate)
+        {
+            double futureValue = 0.0;
+            try
+            {
+                int months = Int32.Parse(strMonths);
+                double monthlyPayment = Double.Parse(strMonthlyPayment);
+                double monthlyinterestRate = Double.Parse(strMonthlyInterestRate);
+                int i = 1;
+                while (i <= months)
+                {
+                    futureValue = (futureValue + monthlyPayment) * (1 + monthlyinterestRate);
+                    i++;
+                }
+            }
+            catch
+            {
+                return "Invalid input";
+            }
+            return futureValue.ToString("n2");
+        }
+        public static string Calc1(string strMonths, string strMonthlyPayment, string strMonthlyInterestRate)
+        {
+            double futureValue = 0.0;
+            try
+            {
+                int months = Int32.Parse(strMonths);
+                double monthlyPayment = Double.Parse(strMonthlyPayment);
+                double monthlyinterestRate = Double.Parse(strMonthlyInterestRate);
+                int i = 1;
+                do
+                {
+                    futureValue = (futureValue + monthlyPayment) * (1 + monthlyinterestRate);
+                    i++;
+                }
+                while (i <= months);
+            }
+            catch
+            {
+                return "Invalid input";
+            }
+            return futureValue.ToString("n2");
+        }
+        public static string Calc2(string strMonths, string strMonthlyPayment, string strMonthlyInterestRate)
+        {
+            double futureValue = 0.0;
+            try
+            {
+                int months = Int32.Parse(strMonths);
+                double monthlyPayment = Double.Parse(strMonthlyPayment);
+                double monthlyinterestRate = Double.Parse(strMonthlyInterestRate);
+                for (int i= 1; i <= months; i++)
+                {
+                    futureValue = (futureValue + monthlyPayment) * (1 + monthlyinterestRate);
+                }
+            }
+            catch
+            {
+                return "Invalid input";
+            }
+            return futureValue.ToString("n2");
+        }
+        public static string Calc3(string strStart, string strEnd, string strIncrement)
+        {
+            string result = "";
+            try
+            {
+                int start = Int32.Parse(strStart);
+                if (start <= 0) throw new Exception();
+                int end = Int32.Parse(strEnd);
+                if (end <= 0) throw new Exception();
+                int increment = Int32.Parse(strIncrement);
+                if (increment <= 0) throw new Exception();
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+            return result;
+        }
+        public static string Calc4(string strStart, string strEnd, string strIncrement)
+        {
+            string result = "";
+            try
+            {
+                int start = Int32.Parse(strStart);
+                if (start <= 0) throw new Exception();
+                int end = Int32.Parse(strEnd);
+                if (end <= 0) throw new Exception();
+                int increment = Int32.Parse(strIncrement);
+                if (increment <= 0) throw new Exception();
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+            return result;
+        }
+
+        public static string Calc5(string strStart, string strEnd, string strIncrement)
+        {
+            string result = "";
+            try
+            {
+                int start = Int32.Parse(strStart);
+                if (start <= 0) throw new Exception();
+                int end = Int32.Parse(strEnd);
+                if (end <= 0) throw new Exception();
+                int increment = Int32.Parse(strIncrement);
+                if (increment <= 0) throw new Exception();
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+            return result;
+        }
+
+        public static string Calc6(string strNumbers, string strCount)
+        {
+            string result = "";
+            int startIndex = 0;
+
+            try
+            {
+                while (strNumbers = 0)
+                { 
+                int endIndex = strNumbers.IndexOf(' ', startIndex);
+                string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                int number = Int32.Parse(strNumber);
+
+                startIndex = endIndex + 1;
+            }
+            }
+            catch
+            {
+               result = "Invalid input";
+            }
+            return result;
+        }
+
+        public static string Calc7(string strNumbers, string strCount)
+        {
+            string result = "";
+            int startIndex = 0;
+
+            try
+            {
+                do while (strNumbers = 4)
+                {
+                    int endIndex = strNumbers.IndexOf(' ', startIndex);
+                    string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                    int number = Int32.Parse(strNumber);
+
+                    startIndex = endIndex + 1;
+                }
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+            return result;
+        }
+
+        public static string Calc8(string strNumbers, string strCount)
+        {
+            string result = "";
+            int startIndex = 0;
+
+            try
+            {
+                for (strNumbers = 0)
+                {
+                    int endIndex = strNumbers.IndexOf(' ', startIndex);
+                    string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                    int number = Int32.Parse(strNumber);
+
+                    startIndex = endIndex + 1;
+                }
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+            return result;
+        }
+    }
+}
